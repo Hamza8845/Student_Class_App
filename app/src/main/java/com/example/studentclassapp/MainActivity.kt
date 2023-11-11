fddb496ebaa6c8f99f5227b8eee9424d12146a75
Ameer Hamza <lasharigroup.officially@gmail.com>
2023-11-11 19:04:58 +0500

    code from talha

--- app/src/main/java/com/example/studentclassapp/MainActivity.kt
+++ app/src/main/java/com/example/studentclassapp/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // this is project for hamza
+        // this is project for talha
     }
 }
